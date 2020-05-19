1fe13aeb92437ff6feeef6502e36588bac6c0772
NannyMika <sara.majava83@gmail.com>
2020-05-19 20:21:21 +0200

    something

--- scripts/index.js
+++ scripts/index.js
@@ -4,12 +4,214 @@ $(document).ready(function(){
         arrows: false,
         dots: true, 
     });
+
     $('.slider_container_2').slick({
-        infinite: true,
-        dots: false, 
-        arrows: true,
         slidesToShow: 3,
-        slidesToScroll: 3
-      });
+        slidesToScroll: 1,
+        dots: true, 
+        arrows: true, 
+        autoplay: true,
+        autoplaySpeed: 4000,
+    });
+
+    $("a[href^='#']").click(function(e) {
+        e.preventDefault();
+        
+        let position = $($(this).attr("href")).offset().top;
     
+        $("body, html").animate({
+            scrollTop: position
+        }, 1000 );
+    });
+});
+
+let map = new google.maps.Map(document.getElementById('google_map'), {
+    center: {lat: 40.830940, lng: -73.181100},
+    zoom: 12, 
+    disableDefaultUI: true,
+    styles: [
+        {
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#f5f5f5"
+            }
+          ]
+        },
+        {
+          "elementType": "labels.icon",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#616161"
+            }
+          ]
+        },
+        {
+          "elementType": "labels.text.stroke",
+          "stylers": [
+            {
+              "color": "#f5f5f5"
+            }
+          ]
+        },
+        {
+          "featureType": "administrative.land_parcel",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#bdbdbd"
+            }
+          ]
+        },
+        {
+          "featureType": "poi",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#eeeeee"
+            }
+          ]
+        },
+        {
+          "featureType": "poi",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#757575"
+            }
+          ]
+        },
+        {
+          "featureType": "poi.park",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#e5e5e5"
+            }
+          ]
+        },
+        {
+          "featureType": "poi.park",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#9e9e9e"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#ffffff"
+            }
+          ]
+        },
+        {
+          "featureType": "road.arterial",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#757575"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#dadada"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#616161"
+            }
+          ]
+        },
+        {
+          "featureType": "road.local",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#9e9e9e"
+            }
+          ]
+        },
+        {
+          "featureType": "transit.line",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#e5e5e5"
+            }
+          ]
+        },
+        {
+          "featureType": "transit.station",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#eeeeee"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#c9c9c9"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#9e9e9e"
+            }
+          ]
+        }
+    ]
+}); 
+
+let marker = new google.maps.Marker({
+    position: {lat: 40.830940, lng: -73.181100}, 
+    map: map,
+})
+
+$(function() {
+  $("form[name='get-in-touch']").validate({
+        rules: {
+        name: "required",
+        email: {
+            required: true,
+            email: true
+      },
+    },
+    messages: {
+      name: "Please enter your first and last name",
+      password: {
+        required: "Please provide a password",
+        minlength: "Your password must be at least 5 characters long"
+      },
+      email: "Please enter a valid email address"
+    },
+  });
 });
